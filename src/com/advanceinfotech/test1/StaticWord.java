1fd9ccbd56e10a62fe6b12ab696a24417193959b
Sunzidabeg <sunzidabeg01@gmail.com>
2019-12-28 20:17:27 -0600

    first commit

---
+++ src/com/advanceinfotech/test1/StaticWord.java
@@ -0,0 +1,43 @@
+package com.advanceinfotech.test1;
+
+public class  StaticWord {
+
+	//public static  int b=10;
+	
+	 public static  int a=10;
+	
+	
+	
+	
+	public  void test1()
+	
+	{
+		 
+		System.out.println("Hello World test1"+a);
+		
+	}
+	
+	
+	
+public  void test2()
+	
+	{
+	System.out.println("Hello World test2"+a);
+		
+	}
+public  void test3()
+
+{
+	System.out.println("Hello World test1"+a);
+	
+}
+
+
+static {
+	System.out.println("HI I am from Static block");
+	
+	
+	
+}
+
+}
---
+++ src/com/advanceinfotech/test1/secondClass.java
@@ -0,0 +1,12 @@
+package com.advanceinfotech.test1;
+
+public class secondClass {
+	public static void main(String[] args) {
+		StaticWord sw=new StaticWord();
+		
+		System.out.println("Hello World test"+StaticWord.a);
+		//StaticWord.test1();
+		 
+		sw.test2();
+	}
+}
